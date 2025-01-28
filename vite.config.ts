2852a6233529757ec05bbe7d23b7a7b458169565
claudiabadescu <badescu.claudia@gmail.com>
2025-01-28 16:55:44 +0100

    funk?

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools(), ghPages()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: 'CICDTest',
+  base: '/CICDTest',
 })
